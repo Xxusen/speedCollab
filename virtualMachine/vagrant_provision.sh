34561bb9be155508d8a850c9c4b90bc650a412bd
hadanahm <hussen.adan-ahmed.externe@capgemini.com>
2017-08-03 09:45:03 +0200

    update vagrant_provision.sh - allow remote connections to mysql-server + app's profile on mysql-server created

--- virtualMachine/vagrant_provision.sh
+++ virtualMachine/vagrant_provision.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+
 MYSQL_ROOT_PWD="password"
+MYSQL_SPEEDCOLLAB_USER_NAME="sc_user"
+MYSQL_SPEEDCOLLAB_USER_PASSWORD="password"
+
 
 add-apt-repository ppa:webupd8team/java -y
 apt-get update
 
-echo "Installing Java 8.."
+# Installing Java 8..
 apt-get install -y software-properties-common python-software-properties
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 apt-get install oracle-java8-installer -y
-echo "Setting environment variables for Java 8.."
+# Setting environment variables for Java 8.."
 apt-get install -y oracle-java8-set-default
 
-echo "installing neo4j"
+# Installing neo4j
 wget -O - http://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
 apt-get update
@@ -21,7 +25,12 @@ sed -i -e 's/#dbms.connectors.default_listen_address=0.0.0.0/dbms.connectors.def
 sed -i -e 's/#dbms.connector.bolt.listen_address=:7687/dbms.connector.bolt.listen_address=:7687/g' /etc/neo4j/neo4j.conf
 service neo4j restart
 
-echo "installing mysql-server"
+#Installing mysql-server
 echo "mysql-server mysql-server/root_password password "$MYSQL_ROOT_PWD | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password "$MYSQL_ROOT_PWD | debconf-set-selections
 apt-get install -y mysql-server
+# Allowing mysql-server to answer to other ip addresses not only localhost
+sed -i -e 's/bind-address		= 127.0.0.1/# bind-address		= 127.0.0.1/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+# Creating our software profile on mysql-server and allowing it to connect from anywhere
+mysql --user=root --password=password -e "CREATE USER '"$MYSQL_SPEEDCOLLAB_USER_NAME"'@'%' IDENTIFIED BY '"$MYSQL_SPEEDCOLLAB_USER_PASSWORD"'; GRANT ALL PRIVILEGES ON *.* TO '"$MYSQL_SPEEDCOLLAB_USER_NAME"'@'%' WITH GRANT OPTION;"
+service mysql restart
