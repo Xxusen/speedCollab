04fe9308848be0ce95d4eb917ea22af23d883fd1
hadanahm <hussen.adan-ahmed.externe@capgemini.com>
2017-08-23 21:20:59 +0200

    declared FrontServlet in deployment descriptor and set the path to it

---
+++ app/src/commands/Authenticate.java
@@ -0,0 +1,18 @@
+package commands;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import commands.interfaces.ICommand;
+
+public class Authenticate implements ICommand {
+
+	@Override
+	public JSONObject execute(HttpServletRequest req, HttpServletResponse res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
