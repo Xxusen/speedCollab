1ec1d1c3b9a1da4f796d0153312cb6a695abbfa3
hadanahm <hussen.adan-ahmed.externe@capgemini.com>
2017-09-01 10:42:26 +0200

    minor changes - naming

--- data/db_schema.sql
+++ data/db_schema.sql
@@ -1,6 +1,6 @@
 USE speedCollab_db;
 create table user(
-    iduser      int             not null    auto_increment,
+    id      int             not null    auto_increment,
     name        varchar(50)     not null,
     firstname   varchar(50)     not null,
     email       varchar(255)    not null,
